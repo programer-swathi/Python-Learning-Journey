2896a470f7494e42c4e00a2d7afd9a529e177543
Swathi-code-Portfolio <116089410+programer-swathi@users.noreply.github.com>
2025-02-01 20:27:55 +0530

    Add files via upload

---
+++ if,elif,else example.py
@@ -0,0 +1,8 @@
+vehicles=['bus','car','scooty','bike']
+vehicles.append('lorry')
+print(vehicles)
+for vehicle in vehicles:
+    if vehicle=='scooty':
+        print("wear helmet")
+    else:
+        print("wear seat belt")
