feb5fa196848abfb1bc37bb02fed7c6fbadac972
Swathi-code-Portfolio <116089410+programer-swathi@users.noreply.github.com>
2025-02-01 21:50:20 +0530

    Add files via upload

---
+++ Slice operator.py
@@ -0,0 +1,8 @@
+#slice operator = [start:stop:step]
+
+flowers=['rose','sunflower','hibiscus','marigold','jasmine']
+flowers.append('lotus')
+print(flowers)
+print(flowers[0:6:2])
+text='hello swathi how are you!'
+print(text[0:12])
