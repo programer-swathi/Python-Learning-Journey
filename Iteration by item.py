5e953ef626396b018d88d3e1828b2e8470e68491
Swathi-code-Portfolio <116089410+programer-swathi@users.noreply.github.com>
2025-02-01 20:05:57 +0530

    Add files via upload

---
+++ Iteration by item.py
@@ -0,0 +1,15 @@
+fruits=['apple','orange','mangoes','strawberry','JKS']
+print("Fruit=",fruits)
+
+for fruit in fruits:
+    if fruit=='apple':
+        print("Hey super apple is a fruit")
+    else:
+        print("Ohh No, it's not an apple")
+
+for x in range(0,5):
+    if fruits[x]=='orange':
+        print(fruits[x])
+    else:
+        print("it's not a orange")
+        
