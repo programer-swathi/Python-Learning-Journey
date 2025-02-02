67f89a5d6b115158e591cdc2f084adeeb0fa1c44
Swathi-code-Portfolio <116089410+programer-swathi@users.noreply.github.com>
2025-02-02 14:12:16 +0530

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,28 @@
+#exponential
+def addTwo(x):
+    return (x + 2)**2
+newvalue=addTwo(10)
+print(newvalue)
+
+def subtractTwo(y):
+    return y - 2
+def multiplyTwo(z):
+    return z * 2
+def divideTwo(num):
+    return num / 2
+
+newvalue1=subtractTwo(10)
+newvalue2=multiplyTwo(10)
+newvalue3=divideTwo(10)
+print(newvalue1)
+print(newvalue2)
+print(newvalue3)
+
+#string
+def something():
+    print('hi')
+def printstring(string):
+    print(string)
+printstring('hello')
+printstring('My name is swathi')
+something()
